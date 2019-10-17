0bb450fa4df5b8df2d59c15dac1c4e949fc6d71a
Lance Mahon <lance.mahon@gmail.com>
2019-10-17 19:46:17 -0400

    Changed apiUrl to be a little bit more right

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://intense-garden-72889.herokuapp.com/',
+  production: 'https://intense-garden-72889.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
